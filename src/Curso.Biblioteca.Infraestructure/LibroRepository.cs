6885c6c141e1da3938af9cd2e121a6189bc2a29e
Diego <diego.marquez@epn.edu.ec>
2022-10-31 18:06:23 -0500

    Create repository

--- src/Curso.Biblioteca.Infraestructure/LibroRepository.cs
+++ src/Curso.Biblioteca.Infraestructure/LibroRepository.cs
@@ -7,23 +7,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Curso.Biblioteca.Infraestructure
 {
-    public class AutorRepository : EfRepository<Autor>, IAutorRepository
+    public class LibroRepository : EfRepository<Libro>, ILibroRepository
     {
-        public AutorRepository(BibliotecaDbContext context) : base(context)
+        public LibroRepository(BibliotecaDbContext context) : base(context)
         {
         }
 
         public async Task<bool> ExisteNombre(string nombre)
         {
-            var resultado = await this._context.Set<Autor>()
-    .AnyAsync(x => x.Nombre.ToUpper() == nombre.ToUpper());
+            var resultado = await this._context.Set<Libro>()
+            .AnyAsync(x => x.Nombre.ToUpper() == nombre.ToUpper());
 
             return resultado;
         }
 
         public async Task<bool> ExisteNombre(string nombre, int idExcluir)
         {
-            var query = this._context.Set<Autor>()
+            var query = this._context.Set<Libro>()
     .Where(x => x.Id != idExcluir)
     .Where(x => x.Nombre.ToUpper() == nombre.ToUpper())
     ;
